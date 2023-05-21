36e061ba3d1d2c6a531f5e51784cc4596a5be56e
dmitry-conquer <dmitry.coqnuer@gmail.com>
2023-05-21 13:59:55 +0300

    bug fix

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -3,7 +3,6 @@ import browserSync from "browser-sync";
 import rename from "gulp-rename";
 import newer from "gulp-newer";
 import gulpIf from "gulp-if";
-import del from "del";
 
 export const plugins = {
   replace: replace,
